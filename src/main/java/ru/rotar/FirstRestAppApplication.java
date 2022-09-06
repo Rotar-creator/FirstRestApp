911b52550aa2e1716bedf6f23f30b6d2313850c4
Михаил <rotari733@gmail.com>
2022-09-06 23:09:49 +0300

    task pul reques

--- src/main/java/ru/rotar/FirstRestAppApplication.java
+++ src/main/java/ru/rotar/FirstRestAppApplication.java
@@ -4,7 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+// pull request
 @SpringBootApplication
 public class FirstRestAppApplication {
 
